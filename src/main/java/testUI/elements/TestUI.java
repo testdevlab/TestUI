87814f8fcb1a60f8ff78aa263c0872fdaabc7c12
Alvaro <48132162+alvarolaserna@users.noreply.github.com>
2022-10-24 08:55:29 +0200

    Update src/main/java/testUI/elements/TestUI.java
    Co-authored-by: Alekss Litvinovs <52907932+theRealAlpaca@users.noreply.github.com>

--- src/main/java/testUI/elements/TestUI.java
+++ src/main/java/testUI/elements/TestUI.java
@@ -189,7 +189,7 @@ public class TestUI {
                     return getMobileElementList(
                             getAccesibilityId(accesibilityIdiOS,accesibilityId)).get(index);
                 }
-                return (WebElement) getDriver().findElements(
+                return getDriver().findElements(
                         getAppiumElement(iOSElement, element)).get(index);
             }
             if (!getLocator(accesibilityIdiOS,accesibilityId).isEmpty()) {
